29777e9899031775c8b259a60711634eb97941e9
Ama Ama <quists@hotmail.com>
2023-02-16 18:30:01 +0000

    file structure

--- exchangemate/src/App.js
+++ exchangemate/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import Landing from "./pages/landing/Landing";
+import Contact from "./pages/contact/Contact";
+import Footer from "./components/footer/Footer";
 
-function App() {
+const App = () => {
   return (
-    <header>Under construction</header>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
+
---
+++ exchangemate/src/components/fetchagg/FetchAgg.js
@@ -0,0 +1,9 @@
+import "./fetchagg.css";
+
+function FetchAgg() {
+  return (
+<h1>FetchAgg</h1>
+  );
+};
+
+export default FetchAgg;
