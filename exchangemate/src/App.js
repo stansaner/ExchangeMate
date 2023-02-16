79418a985b39694d2b013051aeec4afe1d1621bd
Stan Saner <stan.saner@hotmail.co.uk>
2023-02-16 18:10:05 +0000

    Test fetch

--- exchangemate/src/App.js
+++ exchangemate/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 
+
+function fetchCurrencies() {
+  fetch(`https://api.polygon.io/v3/reference/tickers?market=fx&active=true&apiKey=VjdfKVQgSP7rYvGkgO1Cu789ZdvAj_ph`)
+  .then(res => res.json())
+  .then(data => {
+      console.log(data);
+  })
+}
+
+
 function App() {
+  fetchCurrencies();
   return (
     <header>Under construction</header>
-  );
+ 
+  )
 }
 
 export default App;
