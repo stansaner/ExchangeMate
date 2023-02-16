7662a66122ad3bdb3b4345850a94bb8ac37d663c
Stan Saner <stan.saner@hotmail.co.uk>
2023-02-16 19:58:47 +0000

    Restructure files and directories

--- src/components/fetchagg/FetchAgg.js
+++
@@ -1,9 +0,0 @@
-import "./fetchagg.css";
-
-function FetchAgg() {
-  return (
-<h1>FetchAgg</h1>
-  );
-};
-
-export default FetchAgg;
--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./landing.css";
 
-import FetchFx from "../../components/fetchfx/FetchFx";
-import FetchAgg from "../../components/fetchagg/FetchAgg";
+import FetchCurrencies from "../../components/fetchCurrencies/fetchCurrencies";
+import FetchAgg from "../../components/fetchagg/fetchAgg";
 
 
 function Landing() {
   return (
     <>
       <h1>landing</h1>
-      <FetchFx />
+      <FetchCurrencies />
       <FetchAgg />
     </>
   );
