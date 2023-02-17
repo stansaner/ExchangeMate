1d9083bf04e132088c4182410a9a8b326ad893d3
Stan Saner <stan.saner@hotmail.co.uk>
2023-02-17 19:35:24 +0000

    Passing currency choice to the parent - Landing

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import "./landing.css";
 
 import FetchCurrencies from "../../components/fetchCurrencies/fetchCurrencies";
@@ -6,12 +7,17 @@ import FetchAgg from "../../components/fetchagg/fetchAgg";
 import Dropdown from "../../components/dropdown/Dropdown";
 
 function Landing() {
+  const [currencyA, setCurrencyA] = useState("USD");
+  const [currencyB, setCurrencyB] = useState("USD");
+
   return (
     <>
       <h1>landing</h1>
-      <Dropdown />
+      <p>First Currency {currencyA}</p>
+      <p>Second Currency {currencyB}</p>
+      <Dropdown currencyA={currencyA} currencyB={currencyB} setCurrencyA={setCurrencyA} setCurrencyB={setCurrencyB}/>
       <FetchCurrencies />
-      <FetchAgg />
+      <FetchAgg currencyA={currencyA} currencyB={currencyB}/>
     </>
   );
 };
