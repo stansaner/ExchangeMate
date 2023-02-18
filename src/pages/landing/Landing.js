81f7032cf9970b7ad88436f3f384ba4cbc7e8740
Stan Saner <stan.saner@hotmail.co.uk>
2023-02-18 23:34:13 +0000

    Added check boxes

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -5,6 +5,7 @@ import "./landing.css";
 // import FetchCurrencies from "../../components/fetchCurrencies/fetchCurrencies";
 import FetchAgg from "../../components/fetchagg/fetchAgg";
 import Dropdown from "../../components/dropdown/Dropdown";
+import Checkboxes from "../../components/checkboxes/checkboxes"
 import ChartedData from "../../components/chartData/chartData";
 
 
@@ -13,6 +14,29 @@ function Landing() {
   const [currencyB, setCurrencyB] = useState("USD");
   const [showChart, setShowChart] = useState(false);
 
+  // Using state variables for the checkboxes choice of data to display in the graphs
+
+  const [checkedClosing, setCheckedClosing] = useState(false);
+  const [checkedHighest, setCheckedHighest] = useState(false);
+  const [checkedLowest, setCheckedLowest] = useState(false);
+  const [checkedOpen, setCheckedOpen] = useState(false);
+  const [checkedVolume, setCheckedVolume] = useState(false);
+
+  const handleChangeClosing = () => {
+    setCheckedClosing(!checkedClosing);
+  };
+  const handleChangeHighest = () => {
+    setCheckedHighest(!checkedHighest);
+  };
+  const handleChangeLowest = () => {
+    setCheckedLowest(!checkedLowest);
+  };
+  const handleChangeOpen = () => {
+    setCheckedOpen(!checkedOpen);
+  };
+  const handleChangeVolume = () => {
+    setCheckedVolume(!checkedVolume);
+  };
 
   // Using setFetchedData function from useState to update fetchedData state
   const [fetchedData, setFetchedData] = useState('');
@@ -28,6 +52,18 @@ function Landing() {
       <p>First Currency {currencyA}</p>
       <p>Second Currency {currencyB}</p>
       <Dropdown currencyA={currencyA} currencyB={currencyB} setCurrencyA={setCurrencyA} setCurrencyB={setCurrencyB}/>
+      <Checkboxes 
+        checkedClosing={checkedClosing}
+        handleChangeClosing={handleChangeClosing}
+        checkedHighest={checkedHighest}
+        handleChangeHighest={handleChangeHighest}
+        checkedLowest={checkedLowest}
+        handleChangeLowest={handleChangeLowest}
+        checkedOpen={checkedOpen}
+        handleChangeOpen={handleChangeOpen}
+        checkedVolume={checkedVolume}
+        handleChangeVolume={handleChangeVolume}
+      />
       <FetchAgg currencyA={currencyA} currencyB={currencyB} onDataFetch={handleData} showChart={showChart} setShowChart={setShowChart}/>
       {showChart && <ChartedData showChart={showChart} setShowChart={setShowChart} fetchedData={fetchedData}/>}
     </>
