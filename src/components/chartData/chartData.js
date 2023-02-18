ea80f73ff93a8fc92c0c1db6379967532de36ef4
Rania Benameur <raniabenameur97@gmail.com>
2023-02-18 18:33:58 +0000

    added code for lines in chart

--- src/components/chartData/chartData.js
+++ src/components/chartData/chartData.js
@@ -26,27 +26,29 @@ const ChartedData = (props) => {
 
         currentObj.t = date;
 
-        console.log(currentObj)
+        //console.log(currentObj)
         //currentObj.date = dates[index];
         //console.log(currentObj);
     }
     
     console.log(results);
 
+    //Other lines - they distort the line graph when added in so I have put them here for now
+    // <Line type="monotone" dataKey="v" name="Trading volume" stroke="#none" />
+    // <Line type="monotone" dataKey="n" name="Number of transactions" stroke="#none" />
+
     return (
         <>
+        <h3>Results</h3>
             <LineChart width={600} height={300} data={results}>
                 <XAxis dataKey="t" domain={['auto', 'auto']} />
                 <YAxis domain={['auto', 'auto']}/>
                 <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-                <Line type="monotone" dataKey="c" name=""stroke="#8884d8" />
-                <Line type="monotone" dataKey="h" stroke="#82ca9d" />
-                <Line type="monotone" dataKey="l" stroke="#8884d8" />
-                <Line type="monotone" dataKey="n" stroke="#82ca9d" />
-                <Line type="monotone" dataKey="o" stroke="#8884d8" />
-                <Line type="monotone" dataKey="t" stroke="#82ca9d" />
-                <Line type="monotone" dataKey="v" stroke="#82ca9d" />
-                <Line type="monotone" dataKey="vw" stroke="#82ca9d" />
+                <Line type="monotone" dataKey="h" name="Highest price" stroke="#82ca9d" />
+                <Line type="monotone" dataKey="l" name="Lowest price" stroke="#8884d8" />
+                <Line type="monotone" dataKey="o" name="Open price" stroke="#8884d8" />
+                <Line type="monotone" dataKey="c" name="Closing price" stroke="#8884d8" />
+                <Line type="monotone" dataKey="vw" name="Volume weighted average price" stroke="#82ca9d" />
                 <Tooltip />
                 <Legend />
             </LineChart>
