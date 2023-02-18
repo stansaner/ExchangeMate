15687f5e87c557171805694b0921a621032686fa
Rania Benameur <raniabenameur97@gmail.com>
2023-02-18 17:01:54 +0000

    fetch request based on the last 7 days works

--- src/components/fetchagg/fetchAgg.js
+++ src/components/fetchagg/fetchAgg.js
@@ -4,10 +4,12 @@ import { useEffect } from "react";
 import "./fetchagg.css";
 
 function FetchAgg(props) {
-  console.log(props.dates);
+  
+  //Testing props passed
+  //console.log(props.dates);
 
   function bringData() {
-    fetch(`https://api.polygon.io/v2/aggs/ticker/C:${props.currencyA}${props.currencyB}/range/1/day/2023-01-02/2023-01-09?adjusted=true&sort=asc&limit=120&apiKey=VjdfKVQgSP7rYvGkgO1Cu789ZdvAj_ph`)
+    fetch(`https://api.polygon.io/v2/aggs/ticker/C:${props.currencyA}${props.currencyB}/range/1/day/${props.dates[0]}/${props.dates[7]}?adjusted=true&sort=asc&limit=120&apiKey=VjdfKVQgSP7rYvGkgO1Cu789ZdvAj_ph`)
     .then(res => res.json())
       .then(data => {
         
