be30f975953118e932844966de5eb4e0b1f0190c
ad660 <ayanahennings@gmail.com>
2023-02-19 12:20:27 +0000

    navigation bar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,36 @@
 import React from "react";
 import "./header.css";
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoins } from '@fortawesome/free-solid-svg-icons'
 
 function Header() {
   return (
     <>
-      <header>
-        <div className="container nav-container">
-          <nav>
+      {/* <nav className="container nav-container nav-style">
+        <ul class="navbar-nav navbar-right">
+          <li>
+            <h4>eXchangeMate</h4>
+          </li>
+          <li>
             <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
             <NavLink to="/contact">Contact</NavLink>
-          </nav>
-        </div>
-      </header>
+          </li>
+        </ul>
+      </nav>
+      <header></header> */}
+      <nav class="navbar">
+      <a class="navbar-brand" href="#">
+        <FontAwesomeIcon icon={faCoins} size="2x" />
+        <h1>e<span class="nav-highlight">X</span>changeMate</h1>
+      </a>
+      <div class="navbar-links">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/contact">Contact us</NavLink>
+      </div>
+    </nav>
     </>
   );
 }
