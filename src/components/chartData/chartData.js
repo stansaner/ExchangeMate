4087964f812a6a11a8e7325885e698fb8d4f23d5
Rania Benameur <raniabenameur97@gmail.com>
2023-02-19 22:33:05 +0000

    charts now responsive to browser window

--- src/components/chartData/chartData.js
+++ src/components/chartData/chartData.js
@@ -3,7 +3,7 @@ import "./chartData.css";
 // Imported moment.js
 import moment from 'moment';
 // Imported Recharts into fetch agg component
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer} from 'recharts';
 
 const ChartedData = (props) => {
 
@@ -38,29 +38,40 @@ const ChartedData = (props) => {
     return (
         <>
         <h3>Results:</h3>
-            <LineChart width={600} height={300} data={results}>
-                <XAxis dataKey="date" domain={['auto', 'auto']} />
-                <YAxis domain={['auto', 'auto']}/>
-                {/* add gridToggle to stroke */}
-                <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-                {props.checkedHighest && <Line type="monotone" dataKey="h" name="Highest price" stroke="#82ca9d" />}
-                {props.checkedLowest && <Line type="monotone" dataKey="l" name="Lowest price" stroke="#FF7F50" />}
-                {props.checkedOpen && <Line type="monotone" dataKey="o" name="Open price" stroke="#A9A9A9" />}
-                {props.checkedClosing && <Line type="monotone" dataKey="c" name="Closing price" stroke="#008080" />}
-                <Tooltip />
-                <Legend />
-            </LineChart>
+            <div className="chartParent">
+                <ResponsiveContainer width='100%' height='100%'>
+                <LineChart width={600} height={300} data={results}>
+                    <XAxis dataKey="date" domain={['auto', 'auto']} />
+                    <YAxis domain={['auto', 'auto']}/>
+                    {/* add gridToggle to stroke */}
+                    <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+                    {props.checkedHighest && <Line type="monotone" dataKey="h" name="Highest price" stroke="#82ca9d" />}
+                    {props.checkedLowest && <Line type="monotone" dataKey="l" name="Lowest price" stroke="#FF7F50" />}
+                    {props.checkedOpen && <Line type="monotone" dataKey="o" name="Open price" stroke="#A9A9A9" />}
+                    {props.checkedClosing && <Line type="monotone" dataKey="c" name="Closing price" stroke="#008080" />}
+                    <Tooltip />
+                    <Legend />
+                </LineChart>
+                </ResponsiveContainer>
+                </div>
 
-            {props.checkedVolume && <LineChart width={600} height={300} data={results}>
-                <XAxis dataKey="date" domain={['auto', 'auto']} />
-                <YAxis domain={['auto', 'auto']}/>
-                {/* add gridToggle to stroke */}
-                <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-                <Line type="monotone" dataKey="v" name="Trading volume" stroke="#00BFFF" />
-                <Tooltip />
-                <Legend />
-            </LineChart>
-            }
+                {props.checkedVolume && <div className="chartParent">
+                    <ResponsiveContainer width='100%' height='100%'>
+                        <LineChart width={600} height={300} data={results}>
+                        <XAxis dataKey="date" domain={['auto', 'auto']} />
+                        <YAxis domain={['auto', 'auto']}/>
+                        {/* add gridToggle to stroke */}
+                        <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+                        <Line type="monotone" dataKey="v" name="Trading volume" stroke="#00BFFF" />
+                        <Tooltip />
+                        <Legend />
+                        </LineChart>
+                    </ResponsiveContainer>
+                </div>
+                }
+
+
+
         </>
     );
 };
