f90285a52ae27f8ebc5fc30ea2717ef52414c6db
ad660 <ayanahennings@gmail.com>
2023-02-19 15:05:23 +0000

    header almost complete. Sending request to test feature

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./header.css";
 import { NavLink } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoins } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoins } from "@fortawesome/free-solid-svg-icons";
+
+
 
 function Header() {
   return (
@@ -22,15 +24,35 @@ function Header() {
       </nav>
       <header></header> */}
       <nav class="navbar">
-      <a class="navbar-brand" href="#">
-        <FontAwesomeIcon icon={faCoins} size="2x" />
-        <h1>e<span class="nav-highlight">X</span>changeMate</h1>
-      </a>
-      <div class="navbar-links">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/contact">Contact us</NavLink>
-      </div>
-    </nav>
+        <a class="navbar-brand" href="#">
+          <FontAwesomeIcon icon={faCoins} size="2x" />
+          <h1>
+            e<span class="nav-highlight">X</span>changeMate
+          </h1>
+        </a>
+        <div class="navbar-links">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/contact">Contact us</NavLink>
+        </div>
+      </nav>
+      {/* Header begins here */}
+      
+      <header>
+        <div className="flex-container">
+          <div className="flex-item header-style">
+            
+            <h1>Welcome to  eXchangeMate</h1>
+            <p>Money talks, and we're listening - compare the top currency exchange rates, all in one place.</p>
+            <button class="currency-button"><a href="">Compare now</a></button>
+          </div>
+          <div className="flex-item image-style">
+          {/* <FontAwesomeIcon icon={faCoins} size="8x" /> */}
+          </div>
+        </div>
+        <div className="wave"></div>
+      </header>
+
+      {/* Header ends here */}
     </>
   );
 }
